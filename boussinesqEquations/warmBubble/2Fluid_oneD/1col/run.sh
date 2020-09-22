a8cd06d9e84cc2458752430f9ed9bc2bf27d571d
statisdisc <will.a.m@hotmail.co.uk>
2020-09-22 09:20:40 +0100

    Minor test case adjustments

--- boussinesqEquations/warmBubble/2Fluid_oneD/1col/run.sh
+++ boussinesqEquations/warmBubble/2Fluid_oneD/1col/run.sh
@@ -12,7 +12,8 @@ blockMesh -case $case
 #cp -r ../../1Fluid/200col/conditionallyAveraged/1col/2 ./0
 #cp 0/u 0/u.stable
 #cp 0/u 0/u.buoyant
-sed -i 's/buoyancyf/bf.sum/g' 0/P
+#sed -i 's/buoyancyf/bf.sum/g' 0/P
+cp -r init/ ./0
 
 setFields
 
@@ -23,7 +24,17 @@ multiFluidBoussinesqFoamDwDtTransfer
 # Plots
 mv 0/P.buoyant 0/Pi.buoyant
 mv 0/P.stable 0/Pi.stable
-for time in 0 500 1000; do
+#writeCellDataxyz sigma.stable
+#writeCellDataxyz sigma.buoyant
+#writeCellDataxyz b.stable
+#writeCellDataxyz b.buoyant
+#writeCellDataxyz u.stable
+#writeCellDataxyz u.buoyant
+#writeCellDataxyz uz.stable
+#writeCellDataxyz uz.buoyant
+#writeCellDataxyz P.stable
+#writeCellDataxyz P.buoyant
+for time in [0-9]*; do
     ../../plots/plotProfiles.sh $case $time
 done
 
--- boussinesqEquations/warmBubble/copyRawData.py
+++ boussinesqEquations/warmBubble/copyRawData.py
@@ -13,15 +13,17 @@ testCases = ["2Fluid_temp"]
 testCases = ["2Fluid_cosineSquaredTransfer"]
 testCases = ["2Fluid_cosineSquaredTransfer_divTransfer"]
 testCases = ["2Fluid_stabilityAnalysis"]
+testCases = ["2Fluid_oneD"]
 #testCases = ["2Fluid"]
 
 resolutions = ["200col", "100col", "50col", "20col", "10col", "5col", "3col", "1col"]
 resolutions = ["5col", "3col", "1col"]
-#resolutions = ["1col"]
+resolutions = ["1col"]
 #resolutions = ["200col"]
 
 times = ["500", "1000"]
-#times = ["0", "100", "200", "300", "400", "500", "600", "700", "800", "900", "1000"]
+times = ["0", "100", "200", "300", "400", "500", "600", "700", "800", "900", "1000"]
+times = range(0,5100,100)
 
 for testCase in testCases:
     for resolution in resolutions:
@@ -31,21 +33,21 @@ for testCase in testCases:
                 folder = os.path.join(folder, resolution)
                 folder = os.path.join(folder, "conditionallyAveraged")
                 folder = os.path.join(folder, "1col")
-                folder = os.path.join(folder, time)
+                folder = os.path.join(folder, str(time))
                 
                 folderRaw = os.path.join(baseFolderRaw, testCase)
                 folderRaw = os.path.join(folderRaw, resolution)
                 folderRaw = os.path.join(folderRaw, "conditionallyAveraged")
                 folderRaw = os.path.join(folderRaw, "1col")
-                folderRaw = os.path.join(folderRaw, time)
+                folderRaw = os.path.join(folderRaw, str(time))
             else:
                 folder = os.path.join(baseFolder, testCase)
                 folder = os.path.join(folder, resolution)
-                folder = os.path.join(folder, time)
+                folder = os.path.join(folder, str(time))
                 
                 folderRaw = os.path.join(baseFolderRaw, testCase)
                 folderRaw = os.path.join(folderRaw, resolution)
-                folderRaw = os.path.join(folderRaw, time)
+                folderRaw = os.path.join(folderRaw, str(time))
             
             if os.path.exists( folderRaw ):
                 os.system("rm {}/*.xyz".format(folderRaw))
